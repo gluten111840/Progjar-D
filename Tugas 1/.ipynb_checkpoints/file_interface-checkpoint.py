97423a84621130d988db8d0e9ce43e9667bd1d43
gluten111840 <bayueka980@gmail.com>
2022-05-11 07:30:29 +0000

    Feat: Tambah fitur delete

--- Tugas 1/.ipynb_checkpoints/file_interface-checkpoint.py
+++ Tugas 1/.ipynb_checkpoints/file_interface-checkpoint.py
@@ -1,4 +1,4 @@
-import os
+import os, os.path
 import json
 import base64
 from glob import glob
@@ -26,9 +26,22 @@ class FileInterface:
         except Exception as e:
             return dict(status='ERROR',data=str(e))
 
+    def delete(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return dict(status='ERROR',data='Parameter kosong!')
+            if (os.path.exists(filename) == False):
+                return dict(status='ERROR',data=f'File {filename} tidak ditemukan')
+            os.remove(filename)
+            return dict(status='OK',data=f'File {filename} berhasil dihapus')
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
 
 
 if __name__=='__main__':
     f = FileInterface()
-    print(f.list())
-    print(f.get(['pokijan.jpg']))
+    # print(f.list())
+    # print(f.get(['pokijan.jpg']))
+
+    # print(f.delete(['pokijan.jpg']))
--- Tugas 1/file_client_cli.py
+++ Tugas 1/file_client_cli.py
@@ -12,6 +12,7 @@ def send_command(command_str=""):
     logging.warning(f"connecting to {server_address}")
     try:
         logging.warning(f"sending message ")
+        command_str += "\r\n\r\n"
         sock.sendall(command_str.encode())
         # Look for the response, waiting until socket is done (no more data)
         data_received="" #empty string
@@ -63,9 +64,20 @@ def remote_get(filename=""):
         print("Gagal")
         return False
 
+def remote_delete(filename=""):
+    command_str=f"DELETE {filename}"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        print(hasil['data'])
+        return True
+    else:
+        print("Gagal")
+        return False
 
 if __name__=='__main__':
     server_address=('172.16.16.101',6666)
-    remote_list()
-    remote_get('donalbebek.jpg')
+    # remote_list()
+    # remote_get('donalbebek.jpg')
 
+    # delete
+    remote_delete('donalbebek.jpg')
